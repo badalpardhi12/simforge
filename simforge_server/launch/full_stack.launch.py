649508655cd138e5c2f69108eea48422e5250d03
badalpardhi12 <pardhibadal19970@gmasil.com>
2026-02-02 14:21:00 -0800

    Fix: Use absolute mesh paths in URDF for Foxglove visualization

--- simforge_server/launch/full_stack.launch.py
+++ simforge_server/launch/full_stack.launch.py
@@ -74,8 +74,8 @@ def generate_launch_description():
     
     urdf_path_arg = DeclareLaunchArgument(
         'urdf_path',
-        default_value='/ros2_ws/assets/ur5e/ur5e.urdf',
-        description='Path to robot URDF file'
+        default_value='/ros2_ws/assets/ur5e/ur5e_foxglove.urdf',
+        description='Path to robot URDF file (with absolute mesh paths for Foxglove)'
     )
     
     # Get launch configurations
